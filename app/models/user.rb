b086f2a2227b5ac6ffe492aa381901712ef4964a
Paolo Payumo & Ravi S. Rāmphal <pair+paopay.rramphal@devbootcamp.com>
2014-04-03 13:39:24 -0700

    added seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :tweets
   validates_uniqueness_of :handle
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+  validates_uniqueness_of :email
 end
